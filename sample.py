e37747c5dfcfe18d195f637bc53d44dcb6179105
Pavithra Solai Jawahar <pavisj@users.noreply.github.com>
2019-05-10 07:31:24 +0530

    Another file

---
+++ sample.py
@@ -0,0 +1,7 @@
+# import the necessary packages
+import cv2
+ 
+# load the image and show it
+image = cv2.imread("/Users/pavithra.solai/Downloads/jagan.jpeg")
+cv2.imshow("original", image)
+cv2.waitKey(0)
